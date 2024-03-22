d2d35c370728a237109640d1e8fd518182afff17
Victor Core <mrvictorcore@gmail.com>
2024-03-22 19:39:33 +0100

    Modificando modelos de datos con ariel

--- MyHomeStock_back/config/db-script.sql
+++ MyHomeStock_back/config/db-script.sql
@@ -8,84 +8,67 @@ USE myhomestock;
 -- Crear tabla de usuarios
 CREATE TABLE IF NOT EXISTS usuarios (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    nombre_usuario VARCHAR(50) NOT NULL,
+
+    nombre VARCHAR(50) NOT NULL,
     contrasena VARCHAR(50) NOT NULL,
-    email VARCHAR(100) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    email VARCHAR(100) NOT NULL
+);
+
+-- Crear tabla de tipos
+CREATE TABLE IF NOT EXISTS tipo_categoria (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    id_usuario INT,
+
+    nombre VARCHAR(50) NOT NULL,
+
+    FOREIGN KEY (id_usuario) REFERENCES usuarios(id)
 );
 
 -- Crear tabla de categorías
 CREATE TABLE IF NOT EXISTS categoria (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    nombre_categoria VARCHAR(50) NOT NULL,
-    usuario_id INT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    FOREIGN KEY (usuario_id) REFERENCES usuarios(id)
-);
-
--- Crear tabla de tipos
-CREATE TABLE IF NOT EXISTS tipo (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nombre_tipo VARCHAR(50) NOT NULL,
-    usuario_id INT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    FOREIGN KEY (usuario_id) REFERENCES usuarios(id)
-);
-
--- Crear tabla de descripciones
-CREATE TABLE IF NOT EXISTS descripcion (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    id_categoria INT,
     id_tipo INT,
-    nombre_descripcion VARCHAR(50) NOT NULL,
-    usuario_id INT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    FOREIGN KEY (usuario_id) REFERENCES usuarios(id),
-    FOREIGN KEY (id_categoria) REFERENCES categoria(id),
-    FOREIGN KEY (id_tipo) REFERENCES tipo(id)
+    id_usuario INT,
+
+    nombre VARCHAR(50) NOT NULL,
+
+    FOREIGN KEY (id_tipo) REFERENCES tipo_categoria(id),
+    FOREIGN KEY (id_usuario) REFERENCES usuarios(id)
 );
 
 -- Crear tabla de productos con columna "favorito"
 CREATE TABLE IF NOT EXISTS producto (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    nombre_producto VARCHAR(100) NOT NULL,
+    id_usuario INT,
     id_categoria INT,
-    id_tipo INT,
-    id_descripcion INT,
+
+    nombre VARCHAR(100) NOT NULL,
+    descripcion VARCHAR(100),
     cantidad_stock INT,
     cantidad_min_mensual INT,
     favorito BOOLEAN DEFAULT FALSE,
-    usuario_id INT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    FOREIGN KEY (usuario_id) REFERENCES usuarios(id),
-    FOREIGN KEY (id_categoria) REFERENCES categoria(id),
-    FOREIGN KEY (id_tipo) REFERENCES tipo(id),
-    FOREIGN KEY (id_descripcion) REFERENCES descripcion(id)
+
+    FOREIGN KEY (id_usuario) REFERENCES usuarios(id),
+    FOREIGN KEY (id_categoria) REFERENCES categoria(id)
 );
 
 -- Crear tabla de compras con todos los datos del producto
 CREATE TABLE IF NOT EXISTS compra (
     id INT AUTO_INCREMENT PRIMARY KEY,
+    id_usuario INT,
+
+    descripcion VARCHAR(100),
+
+    FOREIGN KEY (id_usuario) REFERENCES usuarios(id)
+);
+
+-- Crear tabla de compra_producto con todos los datos del producto
+CREATE TABLE IF NOT EXISTS compra_producto (
+    id_compra INT,
     id_producto INT,
-    nombre_producto VARCHAR(100),
-    id_categoria INT,
-    id_tipo INT,
-    id_descripcion INT,
-    cantidad_stock INT,
-    cantidad_min_mensual INT,
-    favorito BOOLEAN,
-    cantidad_comprar INT,
-    usuario_id INT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    FOREIGN KEY (usuario_id) REFERENCES usuarios(id),
+
+    cantidad INT NOT NULL DEFAULT 0 CHECK (cantidad >= 0),
+
     FOREIGN KEY (id_producto) REFERENCES producto(id),
-    FOREIGN KEY (id_categoria) REFERENCES producto(id_categoria),
-    FOREIGN KEY (id_tipo) REFERENCES producto(id_tipo),
-    FOREIGN KEY (id_descripcion) REFERENCES producto(id_descripcion)
+    FOREIGN KEY (id_compra) REFERENCES compra(id)
 );
