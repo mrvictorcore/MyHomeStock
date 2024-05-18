866fe873a5b4e3468ed150561cc5222fd62dc732
Victor Core <mrvictorcore@gmail.com>
2024-05-18 21:58:16 +0200

    Actualizando el back usuarios

--- MyHomeStock_back/src/controllers/usuario.controller.js
+++ MyHomeStock_back/src/controllers/usuario.controller.js
@@ -1,85 +1,115 @@
 import { Usuario } from '../models/usuario.model.js';
+import { handleResponse, validateFields, validateId } from '../../config/helpers/dbUtils.js';
 
-export const findAll = function(req, res) {
-  Usuario.findAll(function(err, usuario) {
-    console.log('controller')
-    if (err)
-    res.send(err);
-    console.log('res', usuario);
-    res.send(usuario);
-  });
+export const findAll = async (req, res) => {
+    try {
+        const data_usuario = await Usuario.findAll();
+        handleResponse(res, null, data_usuario);
+    } catch (err) {
+        handleResponse(res, err);
+    }
 };
 
-export const existeUsuario = function(req, res){
+export const existeUsuario = async (req, res) => {
     const new_email = req.query.email;
 
-    Usuario.existeUsuario(new_email, function(err, usuario){
-        if(err){
-            res.send(err);
-        } else {
-            res.json(usuario);
+    try {
+        const data_usuario = await Usuario.existeUsuario(new_email);
+        handleResponse(res, null, data_usuario);
+    } catch (err) {
+        handleResponse(res, err);
+    }
+};
+
+export const create = async (req, res) => {
+    const new_usuario = req.body;
+    let errores = [];
+
+    if (!new_usuario || typeof new_usuario !== 'object' || Object.keys(new_usuario).length === 0) {
+        errores.push('No se recibieron los datos completos');
+    }
+
+    if (!errores.length) {
+        let erroresCampos = validateFields(new_usuario, ['nombre', 'apellido', 'email', 'password']);
+        errores = [...errores, ...erroresCampos];
+    }
+
+    if (errores.length) {
+        res.status(400).json({ error: true, message: 'Por favor añada todos los camopos requeridos: ' + errores.join(', ') });
+    } else {
+        try {
+            const data_usuario = await Usuario.create(new_usuario);
+            handleResponse(res, null, data_usuario);
+        } catch (err) {
+            handleResponse(res, err)
         }
-    });
-};
-
-export const create = function(req, res) {
-    const new_usuario = new Usuario(req.body);
-
-   if(req.body.constructor === Object && Object.keys(req.body).length === 0){
-        res.status(400).send({ error:true, message: 'Por favor añada todos los campos requeridos' });
-    }else{
-        Usuario.create(new_usuario, function(err, usuario) {
-            if (err)
-            res.send(err);
-            res.json({error:false,message:"Usuario añadido correctamente!",data:usuario});
-        });
     }
 };
 
+export const findById = async (req, res) => {
+    const idUser = req.params.id;
 
-export const findById = function(req, res) {
-    Usuario.findById(req.params.id, function(err, usuario) {
-        if (err)
-        res.send(err);
-        res.json(usuario);
-    });
-};
-
-
-export const update = function(req, res) {
-    if(req.body.constructor === Object && Object.keys(req.body).length === 0){
-        res.status(400).send({ error:true, message: 'Por favor añada todos los campos requeridos' });
-    }else{
-        Usuario.update(req.params.id, new Usuario(req.body), function(err, usuario) {
-            if (err)
-            res.send(err);
-            res.json({ error:false, message: 'Usuario actualizado correctamente' });
-        });
+    try {
+        const data_usuario = await Usuario.findById(idUser);
+        handleResponse(res, null, data_usuario);
+    } catch (err) {
+        handleResponse(res, err);
     }
-  
 };
 
+export const update = async (req, res) => {
+    const idUser = req.params.id;
+    const updateUser = req.body;
+    let errores = [];
 
-export const remove = function(req, res) {
-  Usuario.remove( req.params.id, function(err, usuario) {
-    if (err)
-    res.send(err);
-    res.json({ error:false, message: 'Usuario successfully deleted' });
-  });
+    const errorIdUser = validateId(idUser);
+    if (errorIdUser) {
+        errores.push(errorIdUser);
+    }
+
+    if (!updateUser || typeof updateUser !== 'object' || Object.keys(updateUser).length === 0) {
+        errores.push('No se recibieron los datos completos');
+    }
+
+    if (errores.length) {
+        res.status(400).json({ error: true, message: 'Por favor añada todos los camopos requeridos: ' + errores.join(', ') });
+    } else {
+        try {
+            const data_usuario = await Usuario.update(idUser, updateUser);
+            handleResponse(res, null, data_usuario);
+        } catch (err) {
+            handleResponse(res, err);
+        }
+    }
 };
 
+export const remove = async (req, res) => {
+    const idUser = req.params.id;
 
-export const login = function(req, res) {
-    const usuario = new Usuario(req.body);
+    const errorIdUser = validateId(idUser);
+    if (errorIdUser) {
+        return res.status(400).json({ error: true, message: errorIdUser});
+    }
 
-    //handles null error 
-   if(req.body.constructor === Object && Object.keys(req.body).length === 0){
-        res.status(400).send({ error:true, message: 'Por favor añada todos los campos requeridos' });
-    }else{
-        Usuario.login(usuario, function(err, usuario) {
-            if (err)
-            res.send(err);
-            res.json({error:false,message:"Ok",data:usuario});
-        });
+    try {
+        const data_usuario = await Usuario.remove(idUser);
+        handleResponse(res, null, data_usuario);
+    } catch (err) {
+        handleResponse(res, err);
+    }
+};
+
+export const login = async (req, res) => {
+    const usuario = req.body;
+
+    if (!usuario || typeof usuario !== 'object' || Object.keys(usuario).length === 0) {
+        res.status(400).json({ error:true, message: 'Por favor añada todos los campos requeridos' });
+    } else {
+        try {
+            const data_usuario = await Usuario.login(usuario);
+            handleResponse(res, null, data_usuario);
+        } catch (err) {
+            handleResponse(res, err);
+        }
     }
 };
--- MyHomeStock_back/src/models/usuario.model.js
+++ MyHomeStock_back/src/models/usuario.model.js
@@ -1,7 +1,7 @@
 import { getConnection } from './../../config/db.config.js';
 
 export class Usuario {
-    constructor() {
+    constructor(usuario) {
         this.id         = usuario.id;
         
         this.nombre     = usuario.nombre;
@@ -10,102 +10,85 @@ export class Usuario {
         this.password   = usuario.password;
     }
 
-    static async existeUsuario (email, result){
-        var dbConn = getConnection();
-        dbConn.query("SELECT * FROM usuario WHERE email = ?", email, function (err, res){
-            dbConn.end();
-            if(err) {
-                console.log("error: ", err);
-                result(err, null);
-            }
-            else {
-                result(null, res);
-            }
-        });
+    static async existeUsuario (email) {
+        const dbConn = getConnection();
+
+        try {
+            const [res] = await dbConn.query("SELECT * FROM usuario WHERE email = ?", email);
+            return res;
+        } catch (err) {
+            throw err;
+        }
     }
 
-    static async create(newEmp, result) {    
-        var dbConn = getConnection();
-        dbConn.query("INSERT INTO usuario set ?", newEmp, function (err, res) {
-            dbConn.end();
-            if(err) {
-                console.log("error: ", err);
-                result(err, null);
-            }
-            else{
-                console.log(res.insertId);
-                result(null, res.insertId);
-            }
-        });           
+    static async create(newEmp) {    
+        const dbConn = getConnection();
+
+        try {
+            const [res] = await dbConn.query("INSERT INTO usuario SET ?", newEmp);
+            return res;
+        } catch (err) {
+            throw err;
+        }
     }
 
-    static async findById(id, result) {
-        var dbConn = getConnection();
-        dbConn.query("Select * from usuario where id = ? ", id, function (err, res) {      
-            dbConn.end();       
-            if(err) {
-                console.log("error: ", err);
-                result(err, null);
-            }
-            else{
-                result(null, res);
-            }
-        });   
+    static async findById(id) {
+        const dbConn = getConnection();
+
+        try {
+            const [res] = await dbConn.query("SELECT * FROM usuario WHERE id = ?", id);
+            return res;
+        } catch (err) {
+            throw err;
+        }
     }
 
-    static async findAll(result) {
-        var dbConn = getConnection();
-        dbConn.query("Select * from usuario", function (err, res) {
-            dbConn.end();
-            if(err) {
-                console.log("error: ", err);
-                result(null, err);
-            }
-            else{
-                console.log('usuario : ', res);  
-                result(null, res);
-            }
-        });   
+    static async findAll() {
+        const dbConn = getConnection();
+
+        try {
+            const [res] = await dbConn.query("SELECT * FROM usuario");
+            return res;
+        } catch (err) {
+            throw err;
+        }
     }
 
-    static async update(id, usuario, result){
-        var dbConn = getConnection();
-        dbConn.query("UPDATE usuario SET nombre=?,apellido=?,email=?,password=? WHERE id = ?", [usuario.nombre,usuario.apellido,usuario.email,usuario.password, id], function (err, res) {
-            dbConn.end();
-            if(err) {
-                console.log("error: ", err);
-                result(null, err);
-            }else{   
-                result(null, res);
-            }
-        }); 
+    static async update(id, usuario){
+        const dbConn = getConnection();
+        const query = `
+            UPDATE usuario
+            SET nombre = ?, apellido = ?, email = ?, password = ?
+            WHERE id = ?
+        `;
+
+        try {
+            const [res] = await dbConn.query(query, [usuario.nombre,usuario.apellido,usuario.email,usuario.password, id]);
+            return res;
+        } catch (err) {
+            throw err;
+        }
     }
 
-    static async remove(id, result){
-        var dbConn = getConnection();
-        dbConn.query("DELETE FROM usuario WHERE id = ?", [id], function (err, res) {
-            dbConn.end();
-            if(err) {
-                console.log("error: ", err);
-                result(null, err);
-            }
-            else{
-                result(null, res);
-            }
-        }); 
+    static async remove(id){
+        const dbConn = getConnection();
+
+        try {
+            const [res] = await dbConn.query("DELETE FROM usuario WHERE id = ?", [id]);
+            return res;
+        } catch (err) {
+            throw err;
+        }
     }
 
-    static async login(user, result) {
-        var dbConn = getConnection();
-        dbConn.query("Select * from usuario where email = ? and password = ?", [user.email, user.password], function (err, res) {    
-            dbConn.end();         
-            if(err) {
-                console.log("error: ", err);
-                result(err, null);
-            }
-            else{
-                result(null, res);
-            }
-        });   
+    static async login(user) {
+        const dbConn = getConnection();
+
+        try {
+            const [res] = await dbConn.query("SELECT * FROM usuario WHERE email = ? AND password = ?", [user.email, user.password]);
+            return res;
+        } catch (err) {
+            throw err;
+        }
     }
 }
